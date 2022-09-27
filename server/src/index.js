1c4cbe13920a12672f60b81413a3cc948394cebf
vmorenomarin <vmorenomarin@gmail.com>
2022-09-27 14:33:29 -0500

    index.js file set-up and running server test in dev mode.

---
+++ server/src/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+
+// require("./database");
+const app = express();
+
+app.set("port", 4000);
+
+/** Middlewares */
+/**  Parse incoming requests with urlencoded payloads */
+app.use(express.urlencoded({ extended: true }));
+
+/**  Parse incoming requests with JSON payloads */
+app.use(express.json());
+
+/** Cross-Origin Resource Sharing. Enable share resources between two domains/servers. */
+app.use(cors({ origin: "*" }));
+
+
+/**  HTTP request logger */
+app.use(morgan("dev"));
+
+/** Use routes to controller methods */
+
+// app.use("/conversor", require("./routes/conversor.route"));
+
+/** Run server */
+app.listen(app.get("port"), () => {
+    console.log(`Server running in ${app.get("port")} port.`);
+  });
